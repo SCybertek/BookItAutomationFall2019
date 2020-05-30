6373ff8da52e14b20770cf4f93e72178d5e81830
SCybertek <snuryyeva@gmail.com>
2020-05-30 14:05:36 -0400

    added utilities

---
+++ src/test/java/com/bookit/utilities/Endpoints.java
@@ -0,0 +1,10 @@
+package com.bookit.utilities;
+
+public class Endpoints {
+    //to have access to different utils
+
+    String DELETE_STUDENT = "/api/students/{id}";
+    String ADD_STUDENT = "/api/students/student";
+    String GET_ALL_BATCHES = "/api/batches";
+
+}
